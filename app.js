3e95041e964140dd13c7316a58b8ac8470fd6944
Jason Wang <wangpiju0420@gmail.com>
2017-05-28 00:01:38 +0800

    Add blockchain status

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var register = require('./routes/register');
 
 var app = express();
-
-
-
 
 // view engine setup
 //app.set('port',  3000);
@@ -28,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/donation.js
+++ public/javascripts/donation.js
@@ -1,3 +1,3 @@
 function test(){
-	console.log('test!!');
+	//console.log('test!!');
 }
